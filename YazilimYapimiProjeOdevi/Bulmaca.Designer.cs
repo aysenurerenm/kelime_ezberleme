3e1463411234231bbf29b134e0f5c80ef4fbae4c
Busenur Çam <232802041@ogr.cbu.edu.tr>
2025-06-03 00:48:40 +0300

    bulmaca tasarım ekranı eklendi

---
+++ YazilimYapimiProjeOdevi/Bulmaca.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace yazilimyapimiprojem
+{
+    partial class Bulmaca
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblAttempts = new System.Windows.Forms.Label();
+            this.wordlength = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.flowLayoutPanelLetters = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.BackColor = System.Drawing.SystemColors.Info;
+            this.txtGuess.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtGuess.Location = new System.Drawing.Point(35, 153);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(259, 38);
+            this.txtGuess.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.IndianRed;
+            this.btnSubmit.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSubmit.Location = new System.Drawing.Point(35, 202);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(259, 41);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Tahmin Et";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblResult.Location = new System.Drawing.Point(367, 153);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 28);
+            this.lblResult.TabIndex = 2;
+            // 
+            // lblAttempts
+            // 
+            this.lblAttempts.AutoSize = true;
+            this.lblAttempts.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAttempts.Location = new System.Drawing.Point(69, 291);
+            this.lblAttempts.Name = "lblAttempts";
+            this.lblAttempts.Size = new System.Drawing.Size(108, 28);
+            this.lblAttempts.TabIndex = 3;
+            this.lblAttempts.Text = "Kalan Hak:";
+            // 
+            // wordlength
+            // 
+            this.wordlength.AutoSize = true;
+            this.wordlength.BackColor = System.Drawing.Color.Transparent;
+            this.wordlength.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.wordlength.Location = new System.Drawing.Point(69, 119);
+            this.wordlength.Name = "wordlength";
+            this.wordlength.Size = new System.Drawing.Size(65, 28);
+            this.wordlength.TabIndex = 4;
+            this.wordlength.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Peru;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 69);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(321, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WORD GAME";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.DeepPink;
+            this.btnRestart.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRestart.Location = new System.Drawing.Point(51, 347);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(199, 58);
+            this.btnRestart.TabIndex = 6;
+            this.btnRestart.Text = "Yeniden Başla";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // flowLayoutPanelLetters
+            // 
+            this.flowLayoutPanelLetters.BackColor = System.Drawing.Color.Yellow;
+            this.flowLayoutPanelLetters.Location = new System.Drawing.Point(327, 326);
+            this.flowLayoutPanelLetters.Name = "flowLayoutPanelLetters";
+            this.flowLayoutPanelLetters.Size = new System.Drawing.Size(418, 94);
+            this.flowLayoutPanelLetters.TabIndex = 7;
+            // 
+            // Bulmaca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanelLetters);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.wordlength);
+            this.Controls.Add(this.lblAttempts);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtGuess);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Bulmaca";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WORD PUZZLE GAME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblAttempts;
+        private System.Windows.Forms.Label wordlength;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelLetters;
+    }
+}
